3420b2cec4222fadf2d3c6549e5e822ed99e5af8
Manishankar Vakta <manishankarvakta@gmail.com>
2022-03-18 14:10:11 +0600

    First Commit

---
+++ src/components/content/Content.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+    return (
+        <div className='mainContent'>
+            <h1>Content</h1>
+            
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 left">
+                        
+                    </div>
+                    <div className="col-md-6">
+                        <p>
+                        Founded on the 2018. Addition is an attentive,
+agile, and trustworthy marketing agency that
+offers an extensive range of services to
+businesses. We strongly believe that our success
+is intertwined with the success of businesses and
+people we serve. At addition bd we work hard to
+be recognized in the economy, and our aim is to
+enable our clients’ businesses to grow.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
